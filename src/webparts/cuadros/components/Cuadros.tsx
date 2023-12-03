48f110427b0db781cb61f26e0f1fa4694e03b957
Ikigai <dave.corredor@gmail.com>
2023-12-03 00:10:19 -0500

    ajuste de estilos

--- src/webparts/cuadros/components/Cuadros.tsx
+++ src/webparts/cuadros/components/Cuadros.tsx
@@ -8,12 +8,12 @@ export default class Cuadros extends React.Component<ICuadrosProps, {}> {
     return (
       <div className={ styles.cuadro}>
         <div className={ styles.cuadroItem}>
-          <a className={ styles.caption} href={this.props.enlace1} data-title={this.props.titulo1} data-description={this.props.texto1}>
+          <a className={ styles.caption} href={this.props.enlace1} target="_blank" data-title={this.props.titulo1} data-description={this.props.texto1}>
             <img src={this.props.enlaceimagen1} alt="Illustration of Vulture" />
           </a>
         </div>
         <div className={ styles.cuadroItem}>
-          <a className={ styles.caption } href={this.props.enlace1} data-title={this.props.titulo2} data-description={this.props.texto2}>
+          <a className={ styles.caption } href={this.props.enlace1} target="_blank" data-title={this.props.titulo2} data-description={this.props.texto2}>
             <img src={this.props.enlaceimagen2} alt="Illustration of Vulture" />
           </a>
         </div>
